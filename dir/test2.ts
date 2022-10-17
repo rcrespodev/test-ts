2d38e5089c0a078ac04518a39d1ae5dd1b792350
rcrespodev <crespodev@gmail.com>
2022-10-17 18:07:34 -0300

    update

---
+++ dir/test2.ts
@@ -0,0 +1,7 @@
+import { TestClass } from "..";
+
+export class Test2 {
+  field1: string;
+}
+
+const p = new TestClass()
--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import { Test2 } from "./dir/test2"
+
 export class TestClass {
     t: string
 }
+
+const p = new Test2()
